75a543370e3d764286f70b8dca341948f02ea5ca
Josué Bustarviejo <j.bustarviejo@gmail.com>
2018-12-14 09:47:24 +0100

    First approach

--- run.py
+++ run.py
@@ -5,18 +5,9 @@ from time import sleep
 import glob
 import re
 
-from picamera import picamera
-import face_recognition
+session = boto3.session.Session(profile_name="keylab")
 
-polly = connectToPolly()
-camera = picamera.PiCamera()
-
-def connectToPolly():
-    boto3.session.Session(profile_name="apilambda")
-    boto3.setup_default_session(profile_name="apilambda")
-    return boto3.client('polly')
-
-def speak(polly, text, format='mp3', voice='Conchita'):
+def speak(text, format='mp3', voice='Conchita'):
     resp = polly.synthesize_speech(OutputFormat=format, Text=text, VoiceId=voice)
     soundfile = open('sound.mp3', 'wb')
     soundBytes = resp['AudioStream'].read()
@@ -27,39 +18,69 @@ def speak(polly, text, format='mp3', voice='Conchita'):
     play(mysong)
     os.remove('sound.mp3')
 
-def capture():
-    try:
-        camera.start_preview()
-        camera.capture('capture.jpg')
-        camera.stop_preview()
-    finally:
-        camera.close()
-
-def recognize():
+def uploadAll():
     people = glob.glob('know_people/*.jpg')
     people_in_capture=[]
+
+    try:
+        response = rekognition.delete_collection(
+            CollectionId='keynos'
+        )
+    except Exception:
+        print("Keynos collection didn't exist")
+    try:
+        rekognition.create_collection(CollectionId='keynos')
+    except Exception:
+        print("Keynos collection already created")
+
+
     for person in people:
-        person_name = re.search('know_people/(.+?).jpg', person)
+        person_name = re.search('know_people/(.+?).jpg', person)[1]
+        file = open(person, 'rb')
 
-        known_image = face_recognition.load_image_file(person)
-        unknown_image = face_recognition.load_image_file("capture.jpg")
+        # object = s3.Object('keylab', "face_recognition/"+person)
+        # ret = object.put(Body=file,Metadata={'FullName': person_name})
 
-        try:
-            known_image_encoding = face_recognition.face_encodings(known_image)[0]
-            unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-        except IndexError:
-            continue
+        response = rekognition.index_faces(CollectionId="keynos", Image={
+            'S3Object': {
+                'Bucket': 'keylab',
+                'Name': "face_recognition/"+person,
+                }
+            },
+            ExternalImageId=person_name
+        )
 
-        results = face_recognition.compare_faces([known_image_encoding], unknown_encoding)
-        if(results):
-            people_in_capture.append(person_name)
-    return people_in_capture
+def detectFaceFromImage():
+    with open("capture.jpg", "rb") as imageFile:
+      f = imageFile.read()
+      b = bytearray(f)
 
-def loop():
-    capture()
-    people_in_picture = recognize()
-    for person in people_in_picture:
-        speak(polly, "Hola "+person)
-    sleep(3)
+    recognized = rekognition.search_faces_by_image(
+		Image={
+			"Bytes": b
+        },CollectionId="keynos"
+    )
 
-loop()
+    detected_faces = []
+
+    for face in recognized["FaceMatches"]:
+        print("Face: "+str(face))
+        detected_faces.append(face["Face"]["ExternalImageId"])
+    return detected_faces
+
+def list_faces():
+    response = rekognition.list_faces(
+        CollectionId='keynos',
+    )
+    print(response)
+
+s3 = session.resource("s3")
+polly = session.client("polly")
+rekognition = session.client("rekognition")
+
+# list_faces()
+
+uploadAll()
+detected_faces = detectFaceFromImage()
+for face in detected_faces:
+    speak("He detectado a "+face)
